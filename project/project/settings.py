adc293195fa8b4bda89d4facf379e1edb86297b8
BOBO-LU <luwenkai0820@gmail.com>
2020-01-29 23:09:17 +0800

    add sayhello

--- project/project/settings.py
+++ project/project/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hant'
+LANGUAGE_CODE = 'zh-hant'
 
 TIME_ZONE = 'UTC+8'
 
