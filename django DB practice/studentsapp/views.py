415043a458d36173c22a9b490797e1d04767fbb9
BOBO-LU <luwenkai0820@gmail.com>
2020-01-31 21:27:40 +0800

    fix student app view.py bug

--- django DB practice/studentsapp/views.py
+++ django DB practice/studentsapp/views.py
@@ -17,29 +17,39 @@ def listall(request):
 
 
 def insert(request):  #新增資料
-    cName = 'melody'
-    cSex =  'F'
-    cBirthday =  '1997-12-26'
-    cEmail = 'bear@superstar.com'
-    cPhone =  '0963245612'
-    cAddr =  '台北市信義路'
-    unit = student.objects.create(cName=cName, cSex=cSex, cBirthday=cBirthday, cEmail=cEmail,cPhone=cPhone, cAddr=cAddr) 
-    unit.save()  #寫入資料庫
-    students = student.objects.all().order_by('-id')  #讀取資料表, 依 id 遞減排序
+    try:
+
+        cName = 'melody'
+        cSex =  'F'
+        cBirthday =  '1997-12-26'
+        cEmail = 'bear@superstar.com'
+        cPhone =  '0963245612'
+        cAddr =  '台北市信義路'
+        unit = student.objects.create(cName=cName, cSex=cSex, cBirthday=cBirthday, cEmail=cEmail,cPhone=cPhone, cAddr=cAddr) 
+        unit.save()  #寫入資料庫
+        students = student.objects.all().order_by('-id')  #讀取資料表, 依 id 遞減排序
+    except:
+        errormessage = "error insert"
     return render(request, "listall.html", locals())
 	
 def modify(request):  #刪除資料
-    unit = student.objects.get(id=5)
-    unit.cName = '張四平'
-    unit.cAddr = '台北市信義路234號'
-    unit.save()  #寫入資料庫
-    students = student.objects.all().order_by('-id')
+    try:
+        unit = student.objects.get(id=5)
+        unit.cName = '張四平'
+        unit.cAddr = '台北市信義路234號'
+        unit.save()  #寫入資料庫
+        students = student.objects.all().order_by('-id')
+    except:
+        errormessage = "error modify"
     return render(request, "listall.html", locals())
 	
 def delete(request,id=None):  #刪除資料
-    unit = student.objects.get(id=5)
-    unit.delete()
-    students = student.objects.all().order_by('-id')
+    try:
+        unit = student.objects.get(id=5)
+        unit.delete()
+        students = student.objects.all().order_by('-id')
+    except:
+        errormessage = "error delete"
     return render(request, "listall.html", locals())
 
 
