306dd0f28f79feea68c8775bb32f8d532bfd9829
BOBO-LU <luwenkai0820@gmail.com>
2020-02-01 22:01:17 +0800

    add gitignore

--- bobo_linebot/application/views.py
+++ bobo_linebot/application/views.py
@@ -10,6 +10,27 @@ from linebot.models import MessageEvent, TextSendMessage, TextMessage, ImageSend
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        """Return the match method once, then stop"""
+        yield self.match
+        raise StopIteration
+    
+    def match(self, *args):
+        """Indicate whether or not to enter a case suite"""
+        if self.fall or not args:
+            return True
+        elif self.value in args: # changed for v1.5, see below
+            self.fall = True
+            return True
+        else:
+            return False
+
+
 @csrf_exempt
 def callback(request):
 
@@ -27,10 +48,27 @@ def callback(request):
         for event in events:
             if isinstance(event, MessageEvent):
                 if isinstance(event.message, TextMessage):
-                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text)) #回應同一個訊息
+                    
                     userid = event.source.user_id
-                    line_bot_api.push_message('U4f9b4c95fcee10fc8c72ad40cbef90ca', TextSendMessage(text=event.message.text+", send by "+event.source.user_id))
-                    line_bot_api.push_message(userid, TextSendMessage(text=event.message.text+", send by "+event.source.user_id))
+                    t = event.message.text.lower()
+                    
+                    print(type(t))
+                    for case in switch(t):
+                        if case('userid'):
+                            line_bot_api.push_message(userid, TextSendMessage(text="userid : "+event.source.user_id))
+                            line_bot_api.push_message('U4f9b4c95fcee10fc8c72ad40cbef90ca', TextSendMessage(text=event.message.text+", send by "+event.source.user_id))
+                            break
+                        if case('test'):
+                            line_bot_api.push_message(userid, TextSendMessage(text='test'))
+                            break
+                        if case('肚子餓') and userid != 'U715b0aba205ddf78123b47ffb8f28f52': #如果是妹妹，就不能說不好的話
+                            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='乾我什麼事'))
+                            break
+                        if case():
+                            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text)) #回應同一個訊息
+                            line_bot_api.push_message('U4f9b4c95fcee10fc8c72ad40cbef90ca', TextSendMessage(text=event.message.text+", send by "+event.source.user_id))
+                            break
+
         return HttpResponse()
     else:
         return HttpResponseBadRequest() 
