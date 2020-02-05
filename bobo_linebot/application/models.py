6aae98c127ae22b6cfac45401f37dcd8687f3241
jesseHsiao <jesse880603@gmail.com>
2020-02-05 14:36:04 +0800

    bobo

--- bobo_linebot/application/models.py
+++ bobo_linebot/application/models.py
@@ -3,6 +3,5 @@ from django.db import models
 class users(models.Model):
     uid = models.CharField(max_length=50, null = False)
     datatest = models.CharField(max_length=50, null = False)
-    last_send_time = models.TimeField()
     def __str__(self):
         return self.uid
